4e0ab109793e5e9b09c31863e4db235b802b967d
Soeren Reichardt <soer3nreichardt@googlemail.com>
2018-02-08 23:19:52 +0100

    implement session and start with graph + table -> cypher mapping

---
+++ caps-flink/src/main/scala/org/opencypher/caps/flink/value/CAPFEntity.scala
@@ -0,0 +1,15 @@
+package org.opencypher.caps.flink.value
+
+import org.opencypher.caps.api.value.CypherValue.{CypherMap, CypherNode, CypherRelationship}
+
+case class CAPFNode(
+   override val id: Long,
+   override val labels: Set[String] = Set.empty,
+   override val properties: CypherMap = CypherMap.empty) extends CypherNode[Long](id, labels, properties)
+
+case class CAPFRelationship (
+   override val id: Long,
+   override val source: Long,
+   override val target: Long,
+   override val relType: String,
+   override val properties: CypherMap = CypherMap.empty) extends CypherRelationship(id, source, target, relType, properties)
