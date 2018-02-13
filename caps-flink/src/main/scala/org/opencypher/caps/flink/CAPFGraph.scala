a520f4b99a06398d5380ea0df41d586a170c5b0e
Soeren Reichardt <soer3nreichardt@googlemail.com>
2018-02-13 18:31:23 +0100

    few implementations and refactoring

--- caps-flink/src/main/scala/org/opencypher/caps/flink/CAPFGraph.scala
+++ caps-flink/src/main/scala/org/opencypher/caps/flink/CAPFGraph.scala
@@ -5,8 +5,11 @@ import org.opencypher.caps.api.graph.PropertyGraph
 import org.opencypher.caps.api.schema.Schema
 import org.opencypher.caps.api.types.{CTNode, CTRelationship}
 import org.opencypher.caps.flink.value.{CAPFNode, CAPFRelationship}
+import org.opencypher.caps.flink.CAPFConverters._
+import org.opencypher.caps.flink.schema.{EntityTable, NodeTable}
 import org.opencypher.caps.impl.record.{CypherRecords, OpaqueField, RecordHeader}
 import org.opencypher.caps.ir.api.expr.Var
+
 trait CAPFGraph extends PropertyGraph with Serializable {
 
   override def nodes(name: String, nodeCypherType: CTNode): CAPFRecords
@@ -21,16 +24,6 @@ trait CAPFGraph extends PropertyGraph with Serializable {
 
   override def union(other: PropertyGraph): CAPFGraph
 
-  def cache(): CAPFGraph
-
-  def persist(): CAPFGraph
-
-  def persist(storageLevel: StorageLevel): CAPFGraph
-
-  def unpersist(): CAPFGraph
-
-  def unpersist(blocking: Boolean): CAPFGraph
-
   override def toString = s"${getClass.getSimpleName}"
 
 }
@@ -40,17 +33,6 @@ object CAPFGraph {
   def empty(implicit capf: CAPFSession): CAPFGraph =
     new EmptyGraph() {
 
-      override def session: CAPFSession = capf
-
-      override def cache(): CAPFGraph = this
-
-      override def persist(): CAPFGraph = this
-
-      override def persist(storageLevel: StorageLevel): CAPFGraph = this
-
-      override def unpersist(): CAPFGraph = this
-
-      override def unpersist(blocking: Boolean): CAPFGraph = this
     }
 
   def create(nodeTable: NodeTable, entityTables: EntityTable*)(implicit capf: CAPFSession): CAPFGraph = {
@@ -59,17 +41,9 @@ object CAPFGraph {
     new CAPFScanGraph(allTables, schema)
   }
 
-  def create(records: CypherRecords, schema: Schema)(implicit capf: CAPFSession): CAPFGraph = {
-    val CAPFRecords = records.asCAPF
-    new CAPFPatternGraph(CAPFRecords, schema)
-  }
+  def create(records: CypherRecords, schema: Schema)(implicit capf: CAPFSession): CAPFGraph = ???
 
-  def create(nodes: Seq[CAPFNode], rels: Seq[CAPFRelationship])(implicit capf: CAPFSession) = {
-    val nodeDS = capf.env.fromCollection(nodes)
-    val relDS = capf.env.fromCollection(rels)
-
-
-  }
+  def create(nodes: Seq[CAPFNode], rels: Seq[CAPFRelationship])(implicit capf: CAPFSession) = ???
 
   def createLazy(theSchema: Schema, loadGraph: => CAPFGraph)(implicit CAPF: CAPFSession): CAPFGraph =
     new LazyGraph(theSchema, loadGraph) {}
@@ -92,26 +66,6 @@ object CAPFGraph {
     override def union(other: PropertyGraph): CAPFGraph =
       lazyGraph.union(other)
 
-    override def cache(): CAPFGraph = {
-      lazyGraph.cache(); this
-    }
-
-    override def persist(): CAPFGraph = {
-      lazyGraph.persist(); this
-    }
-
-    override def persist(storageLevel: StorageLevel): CAPFGraph = {
-      lazyGraph.persist(storageLevel)
-      this
-    }
-
-    override def unpersist(): CAPFGraph = {
-      lazyGraph.unpersist(); this
-    }
-
-    override def unpersist(blocking: Boolean): CAPFGraph = {
-      lazyGraph.unpersist(blocking); this
-    }
   }
 
   sealed abstract class EmptyGraph(implicit val session: CAPFSession) extends CAPFGraph {
@@ -124,7 +78,7 @@ object CAPFGraph {
     override def relationships(name: String, cypherType: CTRelationship): CAPFRecords =
       CAPFRecords.empty(RecordHeader.from(OpaqueField(Var(name)(cypherType))))
 
-    override def union(other: PropertyGraph): CAPFGraph = other.asCAPF
+    override def union(other: PropertyGraph): CAPFGraph = other.asCapf
   }
 
 }
