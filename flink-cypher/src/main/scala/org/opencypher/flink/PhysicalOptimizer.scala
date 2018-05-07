87f292c4ed1070c818a3dd0bbb4d9aa749e77a75
Soeren Reichardt <soer3nreichardt@googlemail.com>
2018-05-07 15:33:35 +0200

    fix(Skip, Limit): implement physical plan rewriting to always preceed skip and limit with orderby

--- flink-cypher/src/main/scala/org/opencypher/flink/PhysicalOptimizer.scala
+++ flink-cypher/src/main/scala/org/opencypher/flink/PhysicalOptimizer.scala
@@ -1,8 +1,9 @@
 package org.opencypher.flink
 
-import org.opencypher.flink.physical.operators.{CAPFPhysicalOperator, Cache, Start, StartFromUnit}
+import org.opencypher.flink.physical.operators._
+import org.opencypher.okapi.ir.api.block.Asc
 import org.opencypher.okapi.ir.api.util.DirectCompilationStage
-import org.opencypher.okapi.trees.TopDown
+import org.opencypher.okapi.trees.{BottomUp, TopDown}
 
 case class PhysicalOptimizerContext()
 
@@ -10,6 +11,21 @@ class PhysicalOptimizer extends DirectCompilationStage[CAPFPhysicalOperator, CAP
 
   override def process(input: CAPFPhysicalOperator)(implicit context: PhysicalOptimizerContext): CAPFPhysicalOperator = {
     InsertCachingOperators(input)
+    PrecedLimitAndSkipWithOrderby(input)
+  }
+
+  object PrecedLimitAndSkipWithOrderby extends (CAPFPhysicalOperator => CAPFPhysicalOperator) {
+    def apply(input: CAPFPhysicalOperator): CAPFPhysicalOperator = {
+      TopDown[CAPFPhysicalOperator] {
+        case skipOrLimit if skipOrLimit.isInstanceOf[Skip] || skipOrLimit.isInstanceOf[Limit] =>
+          val withPreceededChildren: Array[CAPFPhysicalOperator] = skipOrLimit.children.map {
+            case orderby if orderby.isInstanceOf[OrderBy] => orderby
+            case other => OrderBy(other, Seq(Asc(other.header.slots(0).content.key))).withNewChildren(Array(other))
+          }
+          skipOrLimit.withNewChildren(withPreceededChildren)
+        case other => other
+      }
+    }.rewrite(input)
   }
 
   object InsertCachingOperators extends (CAPFPhysicalOperator => CAPFPhysicalOperator) {
