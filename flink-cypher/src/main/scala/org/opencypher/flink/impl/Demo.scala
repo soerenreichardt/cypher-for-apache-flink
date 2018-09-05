e30113fda1ed2693215a9055987bcfb7e4d4268a
Soeren Reichardt <soer3nreichardt@googlemail.com>
2018-09-05 09:49:13 +0200

    add license headers

--- flink-cypher/src/main/scala/org/opencypher/flink/impl/Demo.scala
+++ flink-cypher/src/main/scala/org/opencypher/flink/impl/Demo.scala
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2016-2018 "Neo4j Sweden, AB" [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
 package org.opencypher.flink.impl
 
 import java.sql._
@@ -15,7 +41,7 @@ import org.opencypher.okapi.api.io.conversion.{NodeMapping, RelationshipMapping}
 import org.opencypher.okapi.impl.util.Measurement
 import org.opencypher.okapi.ir.api.configuration.IrConfiguration.PrintIr
 import org.opencypher.okapi.logical.api.configuration.LogicalConfiguration.PrintLogicalPlan
-import org.opencypher.okapi.relational.api.configuration.CoraConfiguration.PrintPhysicalPlan
+import org.opencypher.okapi.relational.api.configuration.CoraConfiguration.{PrintFlatPlan, PrintOptimizedPhysicalPlan, PrintPhysicalPlan}
 
 object Demo extends App {
 
@@ -136,4 +162,96 @@ object OrcDemo extends App {
       |RETURN n.firstName
     """.stripMargin
   ).getRecords.show
+}
+
+object ThesisDemo extends App {
+
+  val musicNodes = Seq(
+    (0L, "Metal"),
+    (1L, "Jazz")
+  )
+
+  val personNodes = Seq(
+    (2L, "Alice", 23),
+    (3L, "Bob", 26)
+  )
+
+  val knowsRels = Seq(
+    (4L, 2L, 3L, 2017)
+  )
+
+  val likesRels = Seq(
+    (5L, 2L, 0L),
+    (6L, 2L, 1L),
+    (7L, 3L, 1L)
+  )
+
+  val session = CAPFSession.local()
+
+  val musicTable = session.tableEnv.fromDataSet(
+    session.env.fromCollection(musicNodes),
+    'ID, 'genre
+  )
+
+  val personTable = session.tableEnv.fromDataSet(
+    session.env.fromCollection(personNodes),
+    'ID, 'name, 'age
+  )
+
+  val knowsTable = session.tableEnv.fromDataSet(
+    session.env.fromCollection(knowsRels),
+    'ID, 'SOURCE, 'TARGET, 'since
+  )
+
+  val likesTable = session.tableEnv.fromDataSet(
+    session.env.fromCollection(likesRels),
+    'ID, 'SOURCE, 'TARGET
+  )
+
+  val musicMapping = NodeMapping
+    .withSourceIdKey("ID")
+    .withPropertyKey("genre")
+    .withImpliedLabel("Music")
+
+  val personMapping = NodeMapping
+    .withSourceIdKey("ID")
+    .withPropertyKeys("name", "age")
+    .withImpliedLabel("Person")
+
+  val knowsMapping = RelationshipMapping
+    .withSourceIdKey("ID")
+    .withSourceStartNodeKey("SOURCE")
+    .withSourceEndNodeKey("TARGET")
+    .withRelType("KNOWS")
+    .withPropertyKey("since")
+
+  val likesMapping = RelationshipMapping
+    .withSourceIdKey("ID")
+    .withSourceStartNodeKey("SOURCE")
+    .withSourceEndNodeKey("TARGET")
+    .withRelType("LIKES")
+
+  val music = CAPFNodeTable.fromMapping(musicMapping, musicTable)
+  val person = CAPFNodeTable.fromMapping(personMapping, personTable)
+  val knows = CAPFRelationshipTable.fromMapping(knowsMapping, knowsTable)
+  val likes = CAPFRelationshipTable.fromMapping(likesMapping, likesTable)
+
+  val graph = session.readFrom(music, person, knows, likes)
+
+  PrintIr.set()
+  PrintLogicalPlan.set()
+  PrintFlatPlan.set()
+  PrintPhysicalPlan.set()
+  PrintOptimizedPhysicalPlan.set()
+
+  val records = graph.cypher(
+    """
+      | MATCH (p:Person)-[:LIKES*1..3]->(m:Music)
+      | WHERE m.genre = 'Metal'
+      | RETURN p.name AS name
+    """.stripMargin).getRecords
+
+  records.asCapf.explain
+  records.show
+
 }
